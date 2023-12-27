ea3fd6568de72acfa1ad10c53d3f0324222f9ad6
AlexRivasMachin <arivas028@ikasle.ehu.eus>
2023-12-27 10:41:16 +0100

    tema de mongo y asi, hay que mirar cuando subes un audio para guardarlo bien.

--- app.js
+++ app.js
@@ -1,9 +1,37 @@
 const express = require('express');
 const mongojs = require('mongojs');
-const db = mongojs('app:r9aqBsP3I4Byyrib@cluster0.wnzdkvb.mongodb.net/grabaciones', ['grabaciones']);
+const db = mongojs('mongodb://localhost:27017/grabaciones', ['grabaciones']);
 const app = express();
 const path = require('path');
 const port = 5000;
+
+//para meterlos en mongo aunque sea solo una vez
+const audioFiles = [
+    {
+        "filename": "57dcdae8dea1270bb922eb53c0c1d58a",
+        "date": 1694187731170
+    },
+    {
+        "filename": "7f85adaba7d9a9e178e8a1bd8fefa9ce",
+        "date": 1694170942916
+    },
+    {
+        "filename": "44ecc096c58ec33c0ee6156dd9024e4c",
+        "date": 1693776011113
+    }
+];
+
+audioFiles.forEach(file => {
+    db.grabaciones.findOne({ filename: file.filename }, (err, doc) => {
+        if (err) {
+            console.log(err);
+        } else if (!doc) {
+            db.grabaciones.insert(file, (err, doc));
+        } else {
+            console.log('File already exists:', doc);
+        }
+    });
+});
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -23,4 +51,34 @@ app.get('/api/list/:user_id', (req, res) => {
     res.sendFile(path.join(__dirname, 'api/list/index.html'));
 });
 
+app.post('uploadAudio', (req, res) => {
+    const audio = req.body;
+    db.grabaciones.insert(audio, (err, doc) => {
+        if (err) {
+            res.send(err);
+        } else {
+            res.json(doc);
+        }
+    });
+});
+
+app.get('/api/play/:id_', (req, res) => {
+    //pillamos el id del audio
+    const id_ = req.params.id_;
+
+    //buscamos el audio en la base de datos
+    db.grabaciones.findOne({ filename: id_ }, (err, doc) => {
+        if (err) {
+            res.send(err);
+        }
+        //si no lo encontramos, devolvemos un 404
+         else if (!doc) {
+            res.status(404).send('No se encontró el fichero de audio');
+        }
+        //si lo encontramos, devolvemos el fichero de audio
+        else {
+            res.json(doc);
+        }
+    });
+});
 app.listen(port, () => console.log(`Listening on port ${port}!`));
