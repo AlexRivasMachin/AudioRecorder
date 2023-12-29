0cc7c644953342957cb92b64636bb4f89b258c1a
gomezbc <77118356+gomezbc@users.noreply.github.com>
2023-12-29 15:37:52 +0100

    interfaz de login

--- app.js
+++ app.js
@@ -35,10 +35,14 @@ audioFiles.forEach(file => {
     });
 });
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/login.html'));
+});
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+app.get('/recorder', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 app.get('/list', handleList);
