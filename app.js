d491cf66570bfba2012aa5ae162e88ce81155771
AlexRivasMachin <arivas028@ikasle.ehu.eus>
2023-12-29 10:02:56 +0100

    api/upload checkear si esta bien

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongojs = require('mongojs');
 const db = mongojs('mongodb://localhost:27017/grabaciones', ['grabaciones']);
 const app = express();
 const path = require('path');
+const fs = require('fs');
 const port = 5000;
 
 //para meterlos en mongo aunque sea solo una vez
@@ -53,6 +54,7 @@ app.get('/api/list/:user_id', (req, res) => {
     res.sendFile(path.join(__dirname, 'api/list/index.html'));
 });
 
+/*
 app.post('/upload/:name', (req, res) => {
     const audio = req.body;
     db.grabaciones.insert(audio, (err, doc) => {
@@ -63,6 +65,7 @@ app.post('/upload/:name', (req, res) => {
         }
     });
 });
+*/
 
 app.post("/upload/:name", (req, res, next) => {
     upload(req, res, async (err) => {
@@ -90,7 +93,7 @@ app.post("/upload/:name", (req, res, next) => {
 
 app.get('/api/play/:filename', (req, res) => {
     //pillamos el id del audio
-    const id_ = req.params.filename;
+    const filename = req.params.filename;
 
     //buscamos el audio en la base de datos
     db.grabaciones.findOne({ filename: filename }, (err, doc) => {
@@ -109,8 +112,41 @@ app.get('/api/play/:filename', (req, res) => {
     });
 });
 
+app.get('/api/delete/:filename', async (req, res,next) => {
+    // Esta función borrara :filename de la carpeta recordings TO-DO
+    // además, lo borrará también de la base de datos. Erabiltzailearen :check
+    // Devolverá como respuesta las últimas 5 grabaciones del usuario :check
+    const filename = req.params.filename;
+    db.grabaciones.findOne({ filename: filename }, (err, doc) => {
+        if (err) {
+            res.send(err);
+        }
+        else if(!doc){
+            res.json({redirectUrl : 'error404.html'});
+        }
+        else{
+            db.grabaciones.remove({ filename: filename }, (err, doc) => {
+                if (err) {
+                    res.send(err);
+                } else {
+                    // Elimina el archivo del sistema de archivos (no se si esta bien)
+                    //he puesto un require fs arriba pero no se si esta bien
+                    fs.unlink(path.join(__dirname, 'recordings', filename), (err) => {
+                        if (err) {
+                            console.error(`Error al eliminar el archivo: ${err}`);
+                        } else {
+                            handleList(req, res);
+                        }
+                    });
+                }
+            });
+        }
+    });
+});
+
 app.listen(port, () => console.log(`Listening on port ${port}!`));
 
+/**FUNCIONES (TO-IMPROVE)**/
 function handleList(req, res) {
     //en mongo date-1 es para ponerlo en orden descendente para que pille los 5 últimos :)
     db.grabaciones.find({}).sort({date: -1}).limit(5).exec((err, docs) => {
@@ -121,3 +157,17 @@ function handleList(req, res) {
         }
     });
 }
+
+const upload = multer({
+    limits: {
+        fileSize: 2500000
+    },
+    fileFilter: function(req, file, cb) {
+        if (file.mimetype !== 'audio/ogg') {
+            return cb(new Error('Solo se permiten archivos de audio en formato ogg'));
+        }
+        cb(null, true);
+    }
+//single para que siempre maneje un solo archivo
+}).single("recording");
+
